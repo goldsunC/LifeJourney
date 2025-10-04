e2414bbfc770273603f64595c65a33cda36f665a
goldsunC <2428022854@qq.com>
2025-10-04 12:12:18 +0800

    更新application-private.yml中的配置信息

---
+++ backend/lifejourney/src/main/java/org/kangning/lifejourney/LifejourneyApplication.java
@@ -0,0 +1,13 @@
+package org.kangning.lifejourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifejourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifejourneyApplication.class, args);
+    }
+
+}
